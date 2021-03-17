29b88b9e56a9609396acb9156cd31b0e7cf79fb2
Pakho Leung <pakholeung37@gmail.com>
2021-03-17 20:36:07 +0800

    chore: init

---
+++ src/routes/index.svelte
@@ -0,0 +1,69 @@
+<script lang="ts">
+  import "virtual:windi.css";
+  import Counter from "$lib/Counter.svelte";
+</script>
+
+<main>
+  <div
+    class="py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-md space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6"
+  >
+    <div class="text-center space-y-2 sm:text-left">
+      <div class="space-y-0.5">
+        <p class="text-lg text-black font-semibold">Erin Lindford</p>
+        <p class="text-gray-500 font-medium">Product Engineer</p>
+      </div>
+      <button
+        class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2"
+      >
+        Message
+      </button>
+    </div>
+  </div>
+  <h1 class="">Hello world!</h1>
+
+  <Counter />
+
+  <p>
+    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
+    apps.
+  </p>
+</main>
+
+<style lang="scss">
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+      Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  }
+
+  main {
+    text-align: center;
+    padding: 1em;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4rem;
+    font-weight: 100;
+    line-height: 1.1;
+    margin: 4rem auto;
+    max-width: 14rem;
+  }
+
+  p {
+    max-width: 14rem;
+    margin: 2rem auto;
+    line-height: 1.35;
+  }
+
+  @media (min-width: 480px) {
+    h1 {
+      max-width: none;
+    }
+
+    p {
+      max-width: none;
+    }
+  }
+</style>
---
+++ vite.config.js
@@ -0,0 +1,15 @@
+// Consult https://vitejs.dev/config/ to learn about these options
+import { join } from "path";
+import { readFileSync } from "fs";
+import { cwd } from "process";
+import WindiCSS from "vite-plugin-windicss";
+
+const pkg = JSON.parse(readFileSync(join(cwd(), "package.json")));
+
+/** @type {import('vite').UserConfig} */
+export default {
+  plugins: [WindiCSS.default()],
+  ssr: {
+    noExternal: Object.keys(pkg.dependencies || {}),
+  },
+};
