831be968ff238f66a7a4041b95ec18aa313c8f98
Pakho Leung <pakholeung37@gmail.com>
2021-03-22 00:40:08 +0800

    feat: add effect to numbers panl

--- src/routes/index/_Numbers.svelte
+++ src/routes/index/_Numbers.svelte
@@ -1,14 +1,50 @@
 <script context="module" lang="ts">
-    import numbers from "./numbers.json";
+    import rawData from "./numbers.json";
+</script>
+
+<script lang="ts">
+    import { onMount } from "svelte";
+
+    type Item = typeof rawData[number] & { value?: number };
+    let data: Item[] = rawData;
+    data.forEach((item) => {
+        item.value = 0;
+    });
+    onMount(() => {
+        const duration = 1500;
+        const interval = 20;
+        const k = (duration * duration) / interval / interval; // k = (d/i)^2
+        let times = 0;
+        let timerId = setInterval(() => {
+            if (times >= duration / interval) {
+                clearInterval(timerId);
+                timerId = null;
+            } else {
+                times++;
+                data.forEach((item) => {
+                    // y = item.n * times^2 * (1 / duration ) / k
+                    item.value = Math.min(
+                        Math.floor((item.n * times * times) / k),
+                        item.n
+                    );
+                });
+                data = [...data];
+            }
+        }, interval);
+
+        return () => {
+            clearInterval(timerId);
+        };
+    });
 </script>
 
 <div class="w-full shadow-md">
     <ul class="flex items-center justify-center">
-        {#each numbers as item}
+        {#each data as item}
             <li class="flex flex-col items-center my-8 px-16">
                 <div class="flex items-start">
                     <div class="text-4xl mr-1 font-thin">
-                        {item.n}
+                        {item.value}
                     </div>
                     <div class="text-sm font-light mt-1">
                         {item.unit}<span class="text-gray-400">+</span>
---
+++ src/routes/index/_新电商玩法.svelte
@@ -0,0 +1,3 @@
+<div class="title-container">
+    <h2>新电商玩法来袭\n 你，抓住红利了吗？</h2>
+</div>
