b219932cbb011131486b24ab8d5f238cd9a86de9
Pakho Leung <pakholeung37@gmail.com>
2021-04-08 17:32:23 +0800

    chore: up deps

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -5,6 +5,8 @@ const sveltePreprocess = require("svelte-preprocess");
 
 /** @type {import('@sveltejs/kit').Config} */
 module.exports = {
+    // Consult https://github.com/sveltejs/svelte-preprocess
+    // for more information about preprocessors
     preprocess: sveltePreprocess(),
     kit: {
         // By default, `npm run build` will create a standard Node app.
